2be159ffeb955d32722a0a066bf79c82dcf0e905
Gareth Daunton <gdaunton@gmail.com>
2017-10-22 14:35:55 -0400

    Added styles and theme.

--- static/js/containers/LoginContainer.js
+++ static/js/containers/LoginContainer.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
+import { Container, Button, Text, Title, H1, Body, Content, Header } from 'native-base';
+import styles from 'BikeShare/styles/login';
+
+import bikeClipArt from 'BikeShare/img/BikeClipArt.png';
 
 const LoginContainer = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Container>
+      <Header>
+        <Body>
+          <Title>RIT Bike Share</Title>
+        </Body>
+      </Header>
+      <Content contentContainerStyle={styles.container}>
+        <Image
+          source={bikeClipArt}
+          style={styles.logo}
+        />
+        <H1 style={styles.title}>RIT Bike Share</H1>
+        <Button style={styles.button}>
+          <Text>Login</Text>
+        </Button>
+      </Content>
+    </Container>
   );
 };
 
---
+++ static/js/styles/login.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignSelf: 'center'
+  },
+  logo: {
+    marginBottom: 24,
+    maxWidth: 155,
+    maxHeight: 100
+  },
+  title: {
+    marginBottom: 80
+  }
+});
