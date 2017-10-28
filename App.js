d52edde19ae06335afcb0a8e3812be1399a6b3c8
Gareth Daunton <gdaunton@gmail.com>
2017-10-28 14:50:39 -0400

    Removed negative top margin on IOS.

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { StyleProvider, Container } from 'native-base';
 
 import getTheme from './native-base-theme/components';
 
+import { isAndroid } from 'BikeShare/utils/platform';
+
 export default class App extends React.Component {
   constructor() {
     super();
@@ -29,9 +31,10 @@ export default class App extends React.Component {
       return <Expo.AppLoading />;
     }
 
+    const marginTop = isAndroid() ? Expo.Constants.statusBarHeight : null;
     return (
       <StyleProvider style={getTheme()}>
-        <Container style={{ marginTop: Expo.Constants.statusBarHeight }}>
+        <Container style={{ marginTop }}>
           <Index />
         </Container>
       </StyleProvider>
