045451f12e321f7c65ed1df4f328ec8e188bbe1b
Gareth Daunton <gdaunton@gmail.com>
2018-02-19 18:05:39 -0500

    how's this?

--- .travis/add-ssh-key.sh
+++ .travis/add-ssh-key.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 eval $(ssh-agent -s)
-ssh-add - <<< ${GITHUB_DEPLOY_KEY}
-expect "Enter passphrase for (stdin):" { send "\r" }
+openssl aes-256-cbc -K $encrypted_797871a259bb_key -iv $encrypted_797871a259bb_iv -in secret_files.tar.gz.enc -out secret_files.tar.gz -d
+tar -zxvf secret_files.tar.gz
+ssh-add github_deploy_key
