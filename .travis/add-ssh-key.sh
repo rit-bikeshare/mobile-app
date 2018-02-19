a9c8b76b1370587d89dbc45ed5cdb3adaffe20b4
Gareth Daunton <gdaunton@gmail.com>
2018-02-19 16:33:22 -0500

    Fix stuff.

--- .travis/add-ssh-key.sh
+++ .travis/add-ssh-key.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ev
-ssh-add - <<< "${GITHUB_DEPLOY_KEY}"
+ssh-add - <<< ${GITHUB_DEPLOY_KEY}
 expect "Enter passphrase"
 send "\r"
 expect eof
--- .travis/exp-detach.sh
+++ .travis/exp-detach.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -ev
+eval $(ssh-agent -s)
 nvm install node
 npm install -g yarn
 yarn install
-yarn run login -u "${EXPO_USERNAME}" -p "${EXPO_PASSWORD}"
+yarn run login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}
 yarn run detach
 react-native link
