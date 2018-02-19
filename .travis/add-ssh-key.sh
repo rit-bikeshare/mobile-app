3896951e8bc983c21a5f4c90f279120967efa742
Gareth Daunton <gdaunton@gmail.com>
2018-02-19 18:08:34 -0500

    now?

--- .travis/add-ssh-key.sh
+++ .travis/add-ssh-key.sh
@@ -3,4 +3,4 @@ set -ev
 eval $(ssh-agent -s)
 openssl aes-256-cbc -K $encrypted_797871a259bb_key -iv $encrypted_797871a259bb_iv -in secret_files.tar.gz.enc -out secret_files.tar.gz -d
 tar -zxvf secret_files.tar.gz
-ssh-add github_deploy_key
+ssh-add secret_files/github_deploy_key
