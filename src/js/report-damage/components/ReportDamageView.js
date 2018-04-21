3cd452b778a8c5a8ebbbfdf59e27da35d2d1cf09
Gareth Daunton <gdaunton@gmail.com>
2018-04-21 01:03:12 -0400

    Fix damage view background.

--- src/js/report-damage/components/ReportDamageView.js
+++ src/js/report-damage/components/ReportDamageView.js
@@ -301,7 +301,7 @@ class ReportDamageView extends React.Component {
     }
 
     return (
-      <Container>
+      <Container style={{ backgroundColor: '#fffff' }}>
         <View
           style={{
             backgroundColor: '#F36E1F',
